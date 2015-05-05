adbd3b7a1044220d7f68ba08b2830c0a838f7c33
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-05 10:50:01 +0100

    seeded admin data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504132540) do
+ActiveRecord::Schema.define(version: 20150505094621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20150504132540) do
     t.text     "profile_pic"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
