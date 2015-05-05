0d197e5a44ab29486432e693252c986a8a5ae658
JamesGotsell <j.e.gotsell@gmail.com>
2015-05-05 17:07:51 +0100

    managed to get a map showing on the coffeeshop index

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,7 +3,21 @@ var myMap = {}
 myMap.initialize = function(){
   var $field = $('.autocomplete')[0];
   var autoComplete = new google.maps.places.Autocomplete($field);
+
+  var latLngObject = new google.maps.LatLng(51.52, -0.115);
+
+  var myOptions = { 
+    center: latLngObject, 
+    zoom:14, 
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+var map = new google.maps.Map(document.getElementById("googleMap"),myOptions);
+
 }
+
+
+
 
 window.onload = myMap.initialize;
 
