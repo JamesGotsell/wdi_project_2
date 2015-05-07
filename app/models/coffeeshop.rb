59ad9e77b907e2c74f8a388567ea9635b50ba122
elenamvk <elena.sanna.mvk@gmail.com>
2015-05-07 12:21:18 +0100

    fixes and average

--- app/models/coffeeshop.rb
+++ app/models/coffeeshop.rb
@@ -1,18 +1,18 @@
 class Coffeeshop < ActiveRecord::Base
-  belongs_to :user
-  has_many :reviews
-  has_and_belongs_to_many :facilities
-  validates :name, presence: true, length: { minimum: 2 }
-  validates :description, length: { maximum: 500 }
-  validates :address, presence: true 
-  geocoded_by :address
-  reverse_geocoded_by :latitude, :longitude
-  after_validation :geocode, :reverse_geocode
-  
-  mount_uploader :picture, CoffeePictureUploader
-  
-#  def calculateAverage
-#    Coffeeshop.reviews.rating
-#  end
+ belongs_to :user
+ has_many :reviews
+ has_and_belongs_to_many :facilities
+ validates :name, presence: true, length: { minimum: 2 }
+ validates :description, length: { maximum: 500 }
+ validates :address, presence: true 
+ geocoded_by :address
+ reverse_geocoded_by :latitude, :longitude
+ after_validation :geocode, :reverse_geocode
+ 
+ mount_uploader :picture, CoffeePictureUploader
+ 
+ def calculateAverage
+   self.reviews.inject(0){|sum , review | sum + review.rating.to_f }  /  self.reviews.length
+ end
 
 end
